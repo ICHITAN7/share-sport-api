6ef2420bd6d12de7f6d7f60b28ddd39ca647e648
Ichitan <114815633+ICHITAN7@users.noreply.github.com>
2025-11-24 15:10:26 +0700

    Add URL facade import in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Filament\Facades\Filament;
 use Filament\Tables\Enums\FiltersLayout;
 use Filament\Tables\Table;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
